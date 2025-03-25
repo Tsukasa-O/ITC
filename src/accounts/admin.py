5a57a435757f87b5e0c1de4f85819964a860ec57
Tsukasa Ohzono <tzumbrella1224@gmail.com>
2025-03-25 23:20:04 +0900

    エラー内容がわからない。urls.mainでaccountsを繋げた途端にエラーが出ました。参照できてないとのことですが、パスが間違えているのでしょうか。タイプミスじゃないと思っているのですが…

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import CustomAdminChangeForm
 
 class UserAdmin(BaseUserAdmin):
+    form = CustomAdminChangeForm
     # 一覧ページ用
     list_display = (
         "email",
@@ -18,18 +20,27 @@ class UserAdmin(BaseUserAdmin):
     ordering = ("email",)
     search_fields = ("email",)
     
-    # 編集ページ用
+    # 編集ページ    
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('権限', {'fields': ('staff','admin',)}),
+        ('プロフィール', {'fields': (
+            'username',
+            'department',
+            'phone_number',
+            'gender',
+            'birthday',
+        )}),
+        ('Permissions', {'fields': ('staff','admin',)}),
     )
-    
-    # 新規登録用
+
+    # 新規登録
     add_fieldsets = (
         (None, {
-            'classes':('wide',),
-            'fields':('email', 'password1', 'password2')
-        }),
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+        ),
     )
 
 admin.site.register(User, UserAdmin)
+#Profileクラスは不要になったのでコメントアウト
+# admin.site.register(Profile)
--- src/accounts/templatetags/accounts_user.py
+++ src/accounts/templatetags/accounts_user.py
@@ -1,9 +1,10 @@
 from django import template
+
 register = template.Library()
 
 @register.filter
 def user_display(user):
     user_display = "ゲスト"
     if user.is_authenticated:
-        user_display = user.profile.username
+        user_display = user.email
     return user_display
